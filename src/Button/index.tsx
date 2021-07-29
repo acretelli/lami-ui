d6ecc215f0ffa6c050807eaa9213b94aa9242794
Anna Fernandes <anna.cbf@gmail.com>
2021-07-29 15:59:08 -0300

    first commit

---
+++ src/Button/index.tsx
@@ -0,0 +1,20 @@
+import React, { HTMLAttributes, ReactNode } from 'react'
+
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  /** Provide a text for the button */
+  children: ReactNode;
+
+  /** Which variant look like for you */
+  variant: 'primary' | 'secondary'
+}
+
+/** A special button */
+export const Button = ({ children, variant = 'primary', ...props}: Props) => {
+  return (
+    <button {...props} style={{ 
+      backgroundColor: variant === 'primary' ? 'blue' : 'gray' 
+      }}>
+      {children}
+    </button>
+  )
+}
